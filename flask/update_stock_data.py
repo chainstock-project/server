e571196cce046ea2084f04372b772fee2dd1c234
padomay1352 <padomay1352@gmail.com>
2021-10-23 20:01:55 +0900

    flask
    stock-data name필드 추가에 따른 스크립트 변경

--- flask/update_stock_data.py
+++ flask/update_stock_data.py
@@ -8,6 +8,7 @@ import pytz
 def update_stock_data():
     #kospi
     kospi_stock_codes = []
+    kospi_stock_names = []
     kospi_stock_amounts = []
     req = requests.get('https://finance.naver.com/sise/sise_market_sum.naver?sosok=0')
     html = req.text
@@ -24,6 +25,10 @@ def update_stock_data():
         for stock_code_alink in stock_code_alinks:
             kospi_stock_codes.append(stock_code_alink.get('href')[22:])
 
+        stock_name_alinks = soup.select('#contentarea > div.box_type_l > table.type_2 > tbody > tr > td:nth-child(2) > a')
+        for stock_name_alink in stock_name_alinks:
+            kospi_stock_names.append(stock_name_alink.get_text())
+
         stock_amounts_tds = soup.select('#contentarea > div.box_type_l > table.type_2 > tbody > tr > td:nth-child(3)')
         for stock_amounts_td in stock_amounts_tds:
             kospi_stock_amounts.append(stock_amounts_td.get_text().replace(',', ""))
@@ -31,6 +36,7 @@ def update_stock_data():
 
     #kosdaq
     kosdaq_stock_codes = []
+    kosdaq_stock_names = []
     kosdaq_stock_amounts = []
     req = requests.get('https://finance.naver.com/sise/sise_market_sum.naver?sosok=1')
     html = req.text
@@ -47,24 +53,28 @@ def update_stock_data():
         for stock_code_alink in stock_code_alinks:
             kosdaq_stock_codes.append(stock_code_alink.get('href')[22:])
 
+        stock_name_alinks = soup.select('#contentarea > div.box_type_l > table.type_2 > tbody > tr > td:nth-child(2) > a')
+        for stock_name_alink in stock_name_alinks:
+            kosdaq_stock_names.append(stock_name_alink.get_text())
+
         stock_amounts_tds = soup.select('#contentarea > div.box_type_l > table.type_2 > tbody > tr > td:nth-child(3)')
         for stock_amounts_td in stock_amounts_tds:
             kosdaq_stock_amounts.append(stock_amounts_td.get_text().replace(',', ""))
         print("kosdaq : ", "stock_code_count=", len(kosdaq_stock_codes), " stock_amount_count=", len(kosdaq_stock_amounts))
 
-    stock_date=datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime("%Y-%m-%d")
+    stock_date=datetime.now(tz=pytz.timezone('Asia/Seoul')). strftime("%Y-%m-%d")
 
     cmd = ["blockchaind", "tx", "blockchain", "create-stock-data"]
 
     # kospi blockchain에 업데이트
     market_type = "kospi"
     for  i in range(len(kospi_stock_codes)):
-        cmd.extend([kospi_stock_codes[i], market_type, kospi_stock_amounts[i], stock_date])
+        cmd.extend([kospi_stock_codes[i], kospi_stock_names[i], market_type, kospi_stock_amounts[i], stock_date])
 
     # kosdaq blockchain에 업데이트
     market_type = "kosdaq"
     for  i in range(len(kosdaq_stock_codes)):
-        cmd.extend([kosdaq_stock_codes[i], market_type, kosdaq_stock_amounts[i], stock_date])
+        cmd.extend([kosdaq_stock_codes[i], kosdaq_stock_names[i], market_type, kosdaq_stock_amounts[i], stock_date])
 
     cmd.extend(["-y", "--from", "root", "--gas=auto","--keyring-backend","test", "--chain-id", "stock-chain"])
     subprocess.call(cmd)
