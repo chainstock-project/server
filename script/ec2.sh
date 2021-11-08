6515557278dcbd401a407c3711e65aa5816ba02f
padomay1352 <padomay1352@gmail.com>
2021-11-08 14:39:06 +0900

    ec2
    경로 업데이트

--- script/ec2.sh
+++ script/ec2.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
 sudo yum -y install git
 sudo yum -y install docker
 sudo service docker start
 sudo usermod -aG docker ec2-user
 
 # 재접속
-git clone https://github.com/chainstock-project/server.git
-cd server
+git clone https://github.com/stockchain-project/stockchain-firstnode.git
+cd stockchain-firstnode
 docker build . -t stock-chain
 docker run -d --name stock-docker -p 80:5000 -p 26656:26656 -p 26657:26657 -p 1337:1337 stock-chain:latest
 docker logs -f stock-docker
